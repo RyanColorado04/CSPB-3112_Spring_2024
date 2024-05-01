bb36d8d371474074d82b14f3ac78a5bc093ab0f4
RyanColorado04 <ryyo1332@colorado.edu>
2024-04-30 23:04:37 -0500

    Fixed SQL models

--- webstore_project/store/models.py
+++ webstore_project/store/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Model: Category
+class Category(models.Model):
+    name = models.CharField(max_length=250, unique=True)  # Defines a character field with a max length of 250 characters that must be unique across all category instances
+    slug = models.SlugField(max_length=250, unique=True)  # Similar to `name`, but used for URL paths. Needs to be unique
+    description = models.TextField(blank=True)  # Text field for category descriptions that allows empty values
+    image = models.ImageField(upload_to='category', blank=True)  # Image field where uploaded images will be stored in the 'category' directory and can be left blank
+
+    def __str__(self):
+        return self.name  # Returns the string representation of the model, which is the category name
+
+
+# Model: Product
+class Product(models.Model):
+    name = models.CharField(max_length=250, unique=True)  # Defines a character field for the product name, must also be unique
+    slug = models.SlugField(max_length=250, unique=True)  # URL-friendly identifier for the product, also unique
+    description = models.TextField(blank=True)  # Large text field for product descriptions, allows empty values
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)  # Cretaes a many-to-one relationship with Category. If a Category is deleted, all associated Products will be deleted
+    price = models.DecimalField(max_digits=10, decimal_places=2)  # Decimal field to store product prices, up to 10 digits in total, with 2 decimal places
+    image = models.ImageField(upload_to='product', blank=True)  # An image field for product images, stored in the 'product' directory, optionally blank
+    stock = models.IntegerField()  # An integer field to keep track of product stock levels
+    available = models.BooleanField(default=True)  # A boolean field to indicate whether the product is available for sale; Defaults = True.
+    created = models.DateTimeField(auto_now_add=True)  # A datetime field that records when the product instance was created. Automatically set when object is first created.
+    updated = models.DateTimeField(auto_now=True)  # A datetime field that records the last time the product instance was updated. Automatically updated every time the object is saved.
+    
+    def __str__(self):
+        return self.name  # String representation of the Product, typically used in the Django admin and in debug messages.
--- webstore_project/webstore_project/settings.py
+++ webstore_project/webstore_project/settings.py
@@ -121,6 +121,11 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_URL = "media/"
+STATICFILES_DIRS = [
+    BASE_DIR / "static", "media"
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
